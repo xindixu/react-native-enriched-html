30828758d9355a75983452ad767e867473e6f273
Xindi Xu <27241590+xindixu@users.noreply.github.com>
2026-09-24 18:20:55 +0000

    build(frontend): [SUPERAPP-2625] Nested Lists: package review fixes

--- lib/typescript/src/web/normalization/prepareHtmlForWeb.d.ts
+++ lib/typescript/src/web/normalization/prepareHtmlForWeb.d.ts
@@ -1,2 +1,3 @@
 export declare function prepareHtmlForWeb(html: string, useHtmlNormalizer: boolean | undefined): string;
+export declare function wrapBareLiContentInParagraph(doc: Document): void;
 //# sourceMappingURL=prepareHtmlForWeb.d.ts.map
